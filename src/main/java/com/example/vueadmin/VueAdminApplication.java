5192fde1812c7b4da151ba981c0b930510393a8a
huangtao <anjo001112@163.com>
2023-12-20 17:26:28 +0800

    feat:跨域

--- src/main/java/com/example/vueadmin/VueAdminApplication.java
+++ src/main/java/com/example/vueadmin/VueAdminApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VueAdminApplication {
+public class VueAdminApplication  {
 
     public static void main(String[] args) {
         SpringApplication.  run(VueAdminApplication.class, args);
---
+++ src/main/java/com/example/vueadmin/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.example.vueadmin.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@ConfigurationProperties("cors-config")
+public class CorsConfig {
+    private CorsConfiguration buildCorsConfiguration() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildCorsConfiguration());
+        return new CorsFilter(source);
+    }
+}
